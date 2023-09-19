20f2cbd6bbfd0acd573b8e94348ffab5afe6bd3e
guillerminaDiaz <guillerminaadiaz@gmail.com>
2023-09-19 14:32:08 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import AboutUs from './components/AboutUs/AboutUs'
 import Servicios from './components/Servicios/Servicios'
 import Equipo from './components/Equipo/Equipo'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <AboutUs/>
       <Servicios/>
       <Equipo/>
+      <Footer/>
       
     </div>
   )
