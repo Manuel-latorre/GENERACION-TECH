e2e6c9241b8165ca8f1d32ff6fe28c2da49f531f
guillerminaDiaz <guillerminaadiaz@gmail.com>
2023-09-27 12:52:04 -0300

    comentarios borrados

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
-import Contacto from './components/Contacto/Contacto'
-import Faqs from './components/Faqs/Faqs'
-
 import { Route, Routes } from 'react-router'
 import Home from './components/Home/Home'
 import QuienesSomos from './components/QuienesSomos/QuienesSomos'
 import Desarrollo from './components/Desarrollo/Desarrollo'
 import Marketing from './components/Marketing/Marketing'
 import Comercio from './components/Comercio/Comercio'
-import AboutUs from './components/AboutUs/AboutUs'
-import Servicios from './components/Servicios/Servicios'
-import Equipo from './components/Equipo/Equipo'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
 function App() {
@@ -22,7 +16,6 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-      
       <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/nosotros' element={<QuienesSomos/>}/>
