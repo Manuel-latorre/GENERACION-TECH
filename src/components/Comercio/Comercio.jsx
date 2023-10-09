81f78eea3f67dd6e77d201c848d729e999490d18
guillerminaDiaz <guillerminaadiaz@gmail.com>
2023-10-09 10:16:41 -0300

    soluciones de los servicios

--- src/components/Comercio/Comercio.jsx
+++ src/components/Comercio/Comercio.jsx
@@ -13,7 +13,22 @@ const info={
     title3:'EXPERIENCIA DE USUARIO OPTIMIZADA',
     p3: 'Nos enfocamos en crear una experiencia de usuario atractiva y fácil de usar en tu tienda en línea. Esto incluye la navegación, la búsqueda de productos y el proceso de pago.',
     title4:'INTEGRACIÓN CON SISTEMAS DE PAGO',
-    p4:'Un servicio de marketing digital se centrará en desarrollar una estrategia sólida y personalizada para tu negocio. Esto implica identificar tus objetivos, público objetivo y competidores para crear una estrategia que maximice los resultados.'
+    p4:'Un servicio de marketing digital se centrará en desarrollar una estrategia sólida y personalizada para tu negocio. Esto implica identificar tus objetivos, público objetivo y competidores para crear una estrategia que maximice los resultados.',
+    solution1:'GESTIÓN DE REDES SOCIALES Y GENERACIÓN DE CONTENIDO',
+    solText1:'Un equipo de gestión de redes sociales y generación de contenido es esencial para construir y mantener una presencia efectiva en línea. Su enfoque en la comunicación, el branding, el marketing y el análisis contribuye al éxito y la visibilidad de la empresa en el mundo digital, donde la interacción con la audiencia es clave para el crecimiento y el impacto positivo en la marca.',
+    //img1:,
+    solution2:'PUBLICIDAD DE MOTORES DE BUSQUEDA (SEO)',
+    solText2:'Un equipo de SEO es esencial para maximizar la visibilidad en línea, atraer tráfico de calidad y mantener la competitividad en el mundo digital. Su enfoque en la optimización de motores de búsqueda es fundamental para el éxito a largo plazo de una empresa en línea, ya que contribuye directamente a la adquisición y retención de clientes.',
+    //img2:,
+    solution3:'EMAIL MARKETING',
+    solText3:'Un equipo de email marketing juega un papel esencial en el mantenimiento de relaciones con los clientes, la adquisición de nuevos clientes y la generación de ingresos. Su capacidad para diseñar, ejecutar y optimizar campañas de email marketing efectivas es clave para el éxito en el marketing digital, ya que permite llegar a la audiencia de manera personalizada y medible.',
+    //img3:,
+    solution4:'MARKETING DE CONTENIDO',
+    solText4:'Un equipo de marketing de contenido es esencial para la construcción de una marca en línea sólida y efectiva. Su capacidad para crear contenido valioso, atraer a la audiencia adecuada y convertir visitantes en clientes potenciales y clientes reales es fundamental para el éxito en el entorno digital altamente competitivo de hoy en día.',
+    //img4:,
+    solution5:'OPTIMIZACIÓN DE MOTORES DE BUSQUEDA (SEM)',
+    solText5:'Un equipo de optimización de motores de búsqueda (SEM) es esencial para maximizar la visibilidad en línea, atraer tráfico calificado y cumplir con los objetivos de marketing en un mundo digital altamente competitivo. Su experiencia en la gestión de campañas publicitarias en línea puede marcar la diferencia en el rendimiento y el éxito de una empresa en línea.',
+    //img5:,
 }
 const Comercio=()=>{
     return (
--- src/components/Desarrollo/Desarrollo.jsx
+++ src/components/Desarrollo/Desarrollo.jsx
@@ -13,7 +13,22 @@ const info={
     title3:'PROPIEDAD TOTAL',
     p3: 'Cuando creas tu sitio web con programación propia, tienes la propiedad total de todo el código y los activos relacionados. Esto significa que no estás limitado por restricciones de licencia y puedes hacer cambios o mejoras en cualquier momento.',
     title4:'MENOS DEPENDENCIA',
-    p4:'No estás atado a las actualizaciones o cambios de políticas de terceros, como ocurre con las plataformas de gestión de contenido (CMS) preconstruidas. Esto te brinda un mayor control sobre la evolución de tu sitio.'
+    p4:'No estás atado a las actualizaciones o cambios de políticas de terceros, como ocurre con las plataformas de gestión de contenido (CMS) preconstruidas. Esto te brinda un mayor control sobre la evolución de tu sitio.',
+    solution1:'DESARROLLO  Y DISEÑO DE SITIOS WEB A MEDIDA',
+    solText1:'El desarrollo y diseño de sitios web a medida no solo son una inversión en la presencia en línea de una empresa, sino también una estrategia para lograr resultados efectivos y duraderos en un entorno digital competitivo. Estos sitios están diseñados para adaptarse y crecer con la empresa, brindando un alto nivel de personalización y control que puede marcar la diferencia en el éxito en línea.',
+    //img1:,
+    solution2:'Diseño de Experiencia e Interfaz de Usuario (UX/UI)',
+    solText2:'El diseño de experiencia de usuario (UX) y el diseño de interfaz de usuario (UI) son esenciales para crear productos digitales exitosos y atractivos. Estos diseños influyen en la satisfacción del usuario, la retención, la conversión y la percepción de la marca. Invertir en UX/UI no solo mejora la experiencia del usuario, sino que también puede tener un impacto significativo en el rendimiento y el éxito de un producto digital.',
+    //img2:,
+    solution3:'MANTENIMIENTO Y SOPORTE DE SOFWARE',
+    solText3:'El mantenimiento y soporte de software son fundamentales para asegurar que las aplicaciones y sistemas digitales sigan siendo efectivos, seguros y relevantes con el tiempo. Estas prácticas son esenciales para mantener la competitividad, proteger los activos digitales y brindar una experiencia positiva a los usuarios. Ignorar el mantenimiento y el soporte puede resultar en costosos problemas y riesgos de seguridad que pueden evitarse fácilmente con un enfoque proactivo.',
+    //img3:,
+    solution4:'ALOJAMIENTO DE WEB HOSTING',
+    solText4:'Un equipo de alojamiento web hosting eficiente es esencial para proporcionar un ambiente en línea seguro, de alto rendimiento y disponible en todo momento. Su experiencia y dedicación contribuyen directamente al éxito y la satisfacción de los usuarios de los sitios web y aplicaciones en línea, lo que es fundamental en un mundo digital altamente competitivo.',
+    //img4:,
+    solution5:'WEBSITE TESTING',
+    solText5:'Un equipo de testing de sitios web desempeña un papel esencial en la creación de una experiencia en línea exitosa y libre de problemas. Su enfoque en la calidad, la seguridad y la funcionalidad asegura que los usuarios puedan interactuar con el sitio de manera efectiva y segura, lo que a su vez contribuye al éxito y la reputación de la empresa en línea.',
+    //img5:,
 }
 const Desarrollo = () => {
     return (
